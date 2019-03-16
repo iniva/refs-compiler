cb8cb6826b186e09a6c3c6d78c52889a1b308712
Tony <tonyblast@gmail.com>
2019-03-16 19:15:49 -0300

    [rewrite-es6] test(processor): Added new jest test. Removed old test

---
+++ src/processor.test.js
@@ -0,0 +1,47 @@
+/* global describe it expect */
+
+import Processor from './processor';
+
+describe('Processor', () => {
+  it('should throw when trying to get a processor without specifying the type', () => {
+    const processor = new Processor();
+
+    expect(() => processor.getProcessor())
+      .toThrowError('You must specify a processor type');
+    expect(() => processor.getProcessor(null))
+      .toThrowError('You must specify a processor type');
+  });
+
+  it('should throw when trying to get an invalid processor', () => {
+    const processor = new Processor();
+
+    expect(() => processor.getProcessor('wrong'))
+      .toThrow('Processor [wrong] is invalid');
+  });
+
+  it('should return a processor when given a valid type', () => {
+    const processor = new Processor();
+    const expectedProperties = ['process', 'dump', 'write'];
+
+    const yamlProcessor = processor.getProcessor('yaml');
+    const ymlProcessor = processor.getProcessor('yml');
+    const jsonProcessor = processor.getProcessor('json');
+    const iniProcessor = processor.getProcessor('ini');
+
+    expect(typeof yamlProcessor).toBe('object');
+    expect(Object.keys(yamlProcessor))
+      .toEqual(expect.arrayContaining(expectedProperties));
+
+    expect(typeof ymlProcessor).toBe('object');
+    expect(Object.keys(ymlProcessor))
+      .toEqual(expect.arrayContaining(expectedProperties));
+
+    expect(typeof jsonProcessor).toBe('object');
+    expect(Object.keys(jsonProcessor))
+      .toEqual(expect.arrayContaining(expectedProperties));
+
+    expect(typeof iniProcessor).toBe('object');
+    expect(Object.keys(iniProcessor))
+      .toEqual(expect.arrayContaining(expectedProperties));
+  });
+});
