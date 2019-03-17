5c2e61c52a5e9578bafc64c4ba488f0e78a09fdb
Tony <tonyblast@gmail.com>
2019-03-17 11:30:57 -0300

    [rewrite-es6] refactor(src/index): Refactored into async/await

--- src/index.js
+++ src/index.js
@@ -3,54 +3,57 @@ import fs from 'fs';
 
 import Processor from './processor';
 
-const compile = async (inputFile, outputFile) => new Promise((resolve, reject) => {
-  try {
-    if (!inputFile) {
-      return reject(new Error('No template file provided'));
-    }
-
-    const fileExtension = inputFile.split('.').pop();
-
-    if (!['yaml', 'yml', 'json', 'ini'].includes(fileExtension)) {
-      return reject(new Error('No valid config found'));
-    }
-
-    const processor = Processor.getProcessor(fileExtension);
-    const inputFilePath = path.resolve(inputFile);
-
-    try {
-      fs.statSync(inputFilePath);
-    } catch (err) {
-      return reject(new Error(`Path does not exist: ${inputFilePath}`));
-    }
-
-    return processor.process(inputFilePath)
-      .then(contentPkg => {
-        try {
-          const compiled = JSON.parse(contentPkg.dataString);
-
-          if (outputFile) {
-            const outputFilePath = path.resolve(outputFile);
-            return processor.write(outputFilePath, compiled);
-          }
-
-          return processor.dump(compiled);
-        } catch (err) {
-          return reject(new Error(`An error occurred while parsing JSON string: ${err.message}`));
-        }
-      })
-      .then(results => {
-        resolve({
-          outputFile: results.outputFile || null,
-          content: results.content || null,
-        });
-      })
-      .catch(err => {
-        reject(err);
-      });
-  } catch (err) {
-    return reject(err);
+const compile = async (inputFile, outputFile) => {
+  if (!inputFile) {
+    throw new Error('No template file provided');
   }
-});
+
+  const fileExtension = inputFile.split('.').pop();
+
+  if (!['yaml', 'yml', 'json', 'ini'].includes(fileExtension)) {
+    throw new Error('No valid config found');
+  }
+
+  const processor = (new Processor()).getProcessor(fileExtension);
+  const inputFilePath = path.resolve(inputFile);
+
+  try {
+    fs.statSync(inputFilePath);
+  } catch (err) {
+    throw new Error(`Path does not exist: ${inputFilePath}`);
+  }
+
+  let contentPkg = null;
+
+  try {
+    contentPkg = await processor.process(inputFilePath);
+  } catch (err) {
+    throw err;
+  }
+
+  try {
+    let results = null;
+    const compiled = JSON.parse(contentPkg.dataString);
+
+    if (outputFile) {
+      const outputFilePath = path.resolve(outputFile);
+      results = await processor.write(outputFilePath, compiled);
+
+      return {
+        outputFile: results.outputFile || null,
+        content: results.content || null,
+      };
+    }
+
+    results = processor.dump(compiled);
+
+    return {
+      outputFile: results.outputFile || null,
+      content: results.content || null,
+    };
+  } catch (err) {
+    throw new Error(`An error occurred while parsing JSON string: ${err.message}`);
+  }
+};
 
 export default compile;
